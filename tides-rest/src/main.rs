f712f96532658c86cf3feb4f7bcd99d2a58cdc83
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2020-12-21 17:32:53 +0100

    Reconstruct signal using fft

--- tides-rest/src/main.rs
+++ tides-rest/src/main.rs
@@ -1,10 +1,21 @@
 // use tides_rest::stations::Station;
-use tides_rest::measurements::Measurement;
+// use tides_rest::measurements::Measurement;
+use tides_signals;
+use std::f32::consts::PI;
 
 fn main() -> Result<(), minreq::Error> {
-    let data = Measurement::query_station_id(
-        "d3f822a0-e201-4a61-8913-589c74818ae0", 1)?;
-    println!("{:#?}", data);
+    // let data = Measurement::query_station_id(
+    //     "d3f822a0-e201-4a61-8913-589c74818ae0", 1)?;
+    // println!("{:#?}", data);
 
+    let fun = |t: f32| (PI * t / 20f32).cos() + (1.5 * PI * t / 20f32).cos() + 3.14;
+    let samples : Vec<f32> = (0 .. 40).map(|i| fun(i as f32)).collect();
+
+    let signal = tides_signals::reconstruct(&samples);
+    
+    let cmp : Vec<(f32, f32)> = (0 .. 40)
+        .map(|i| (fun(i as f32), signal(i as f32)))
+        .collect();
+    println!("{:#?}", cmp);
     Ok(())
 }
--- tides-signals/src/lib.rs
+++ tides-signals/src/lib.rs
@@ -1,7 +1,69 @@
+use std::f32::consts::PI;
+use rustfft::FFTplanner;
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+
+fn compute_fft(values : &Vec<f32>) -> Vec<Complex<f32>>
+{
+    let mut input : Vec<Complex<f32>> = values
+        .iter()
+        .map(|val| Complex::from(val))
+        .collect();
+
+    let len = input.len();
+
+    let mut output : Vec<Complex<f32>> = vec![Complex::zero(); len];
+
+    let mut planner = FFTplanner::new(false);
+    let fft = planner.plan_fft(len);
+    fft.process(&mut input, &mut output);
+
+    output
+}
+
+pub fn reconstruct(samples : &Vec<f32>) -> impl Fn(f32) -> f32 {
+    let len = samples.len() as f32;
+    let fft = compute_fft(&samples);
+    move |t : f32| fft.iter()
+        .enumerate()
+        .map(|(k, amplitude)| {
+            let phase = 2f32 * PI * k as f32 / len;
+            amplitude * (Complex::from(phase * t) * Complex::i()).exp()
+        })
+        .sum::<Complex<f32>>()
+        .re / len
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::f32::consts::PI;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_fft() {
+        let fun = |x: f32| (PI * x).cos();
+        let samples : Vec<f32> = (0 .. 20).map(|i| fun(i as f32)).collect();
+
+        // Just test that it doesn't panic
+        compute_fft(&samples);
+    }
+
+    #[test]
+    fn test_reconstruction() {
+        let fun = |t: f32|
+            (PI * t / 20f32).cos()
+            + 2f32 * (0.5 * PI * t / 20f32).cos()
+            + 3f32 * (1.5 * PI * t / 20f32).cos()
+            + 3.14;
+        let samples : Vec<f32> = (0 .. 40).map(|i| fun(i as f32)).collect();
+
+        let reconstructed = reconstruct(&samples);
+        let predictions : Vec<f32> = (0 .. 40)
+            .map(|i| reconstructed(i as f32))
+            .collect();
+
+        for (expected, actual) in samples.into_iter().zip(predictions) {
+            assert!((expected - actual).abs() < 1e-3)
+        }
     }
 }
