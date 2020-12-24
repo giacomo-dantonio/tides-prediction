27b809105ca6c24a7678e6b23254dec6ca4c605e
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2020-12-24 21:46:52 +0100

    finalize wasm api

--- tides-signals/src/measurements.rs
+++ tides-signals/src/measurements.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
-use serde_json;
-use chrono::{self, DateTime, Utc};
+use chrono::{self, DateTime, NaiveDateTime, Utc};
 use wasm_bindgen::prelude::*;
+use crate::predictions;
 
 /// Represent a station measurement that can be queried on pegelonline.
 #[derive(Deserialize, Debug)]
@@ -11,16 +11,41 @@ pub struct Measurement {
 }
 
 #[wasm_bindgen]
-impl Measurement {
-    pub fn get_number() -> u32 {
-        42
+pub struct Series {
+    measurements: Vec<Measurement>,
+    signal: Box<dyn Fn(DateTime<Utc>) -> f32>
+}
+
+#[wasm_bindgen]
+impl Series {
+    pub fn from_json(json_str: &str) -> Series {
+        let measurements = serde_json::from_str(json_str)
+            .unwrap_or(vec![]);
+        let signal = Box::new(predictions::reconstruct(&measurements)); 
+
+        Series {
+            measurements,
+            signal
+        }
     }
 
-    pub fn from_json(json_str: &str) -> Vec<f32> {
-        let data : Vec<Measurement> = serde_json::from_str(json_str)
-            .unwrap_or(vec![]);
-        data.iter()
+    pub fn times(&self) -> Vec<i64> {
+        self.measurements
+            .iter()
+            .map(|mes| mes.timestamp.timestamp())
+            .collect()
+    }
+
+    pub fn values(&self) -> Vec<f32> {
+        self.measurements
+            .iter()
             .map(|mes| mes.value)
             .collect()
+    }
+
+    pub fn evaluate(&self, timestamp: i64) -> f32 {
+        let dt = DateTime::<Utc>::from_utc(
+            NaiveDateTime::from_timestamp(timestamp, 0), Utc);
+        (self.signal)(dt)
     }
 }
--- tides-wasm/tests/web.rs
+++ tides-wasm/tests/web.rs
@@ -5,19 +5,10 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
-use tides_signals::measurements::Measurement;
+use tides_signals::measurements::Series;
 use tides_wasm::utils;
 
 wasm_bindgen_test_configure!(run_in_browser);
-
-#[wasm_bindgen_test]
-fn pass() {
-    utils::set_panic_hook();
-
-    let data = Measurement::get_number();
-    assert_eq!(data, 42);
-}
-
 
 #[wasm_bindgen_test]
 fn test_json() {
@@ -33,6 +24,12 @@ fn test_json() {
             \"value\": 466
         }
     ]";
-    let data = Measurement::from_json(json_str);
-    assert_eq!(data, vec![393f32, 466f32]);
+    let series = Series::from_json(json_str);
+    let times = series.times();
+    let values = series.values();
+
+    for (timestamp, value) in times.into_iter().zip(values) {
+        let prediction = series.evaluate(timestamp);
+        assert!((prediction - value).abs() <= 0.1);
+    }
 }
